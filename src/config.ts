7a4b06fb3e8bc9d9473ef754bc1df4fbed16869a
Tse-Yue <Maxmins1231@outlook.com>
2025-10-04 09:35:24 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,12 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/Tse-Yue", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
+		{
+			name: "洛谷",
+			url: "https://www.luogu.com.cn/user/762100", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
